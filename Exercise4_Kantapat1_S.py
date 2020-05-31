dd06622ba30158bde03eab86039a52926c294b76
artkantapat <34628251+artkantapat@users.noreply.github.com>
2020-05-31 13:44:56 +0700

    Create Excerise 4
    Create Excerise 4

---
+++ Exercise4_Kantapat1_S.py
@@ -0,0 +1,10 @@
+a=10
+b=10.2
+c=35
+d=60
+
+print("--- Your Score ---")
+print("English : ",a)
+print("General Business : ",b)
+print("Introduction to Computer Systems : ",c)
+print("Computer Programming : ",d)
